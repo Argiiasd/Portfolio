9556c71f7f62450c5fc3a63b26b68f32ba2eca5e
Argiiasd <mateo.p.giacosa@gmail.com>
2023-10-29 23:07:55 -0300

    cambios en tamaños de iconos

--- Portfolio/src/Components/Presentation/Presentation.jsx
+++ Portfolio/src/Components/Presentation/Presentation.jsx
@@ -14,10 +14,10 @@ const Presentation = () => {
                 <h3 className={styles.Job}>Desarrollador Web <span className={styles.Job_2}>Full-Stack</span></h3>
                 <div className={styles.Social_Media}>
                     <NavLink to={"https://www.linkedin.com/in/mateo-jeremias-pg/"}>
-                        <LinkedInIcon className={styles.Icon} sx={{color: '#333333', fontSize: 50}}/>    
+                        <LinkedInIcon className={styles.Icon} sx={{color: '#333333', fontSize: 80}}/>    
                     </NavLink>
                     <NavLink to={"https://github.com/Argiiasd"}>
-                        <GitHubIcon className={styles.Icon} sx={{color: '#333333', fontSize: 50}}/>    
+                        <GitHubIcon className={styles.Icon} sx={{color: '#333333', fontSize: 80}}/>    
                     </NavLink>
                 </div>
                 {/* <img src={Imagen} alt="" className={styles.Imagen}/> */}
