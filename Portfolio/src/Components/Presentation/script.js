0ab7a8c503cb4aeb4865389668c7a8048d0826a0
Argiiasd <mateo.p.giacosa@gmail.com>
2023-10-31 17:34:19 -0300

    prueba de animacion en sobre mi

---
+++ Portfolio/src/Components/Presentation/script.js
@@ -0,0 +1,13 @@
+const element = document.querySelectorAll('.')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+}, {
+    threshold: 0.5
+})
